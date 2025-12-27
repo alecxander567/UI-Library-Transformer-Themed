82c6fb5135b2660fb8895b488eda2b02b28fbe50
alecxander567 <alecxanderespaldon21@gmail.com>
2025-12-27 13:29:29 +0800

    Few modifications

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,13 @@ import { Button } from "./components";
 
 function App() {
   return (
-    <div style={{ display: "flex", gap: "12px", flexWrap: "wrap" }}>
+    <div
+      style={{
+        display: "grid",
+        gap: "12px",
+        gridAutoFlow: "row",
+        justifyItems: "start", 
+      }}>
       <Button label="Prime" />
       <Button label="Ironhide" variant="ironhide" />
       <Button label="Sideswipe" variant="sideswipe" />
@@ -14,6 +20,7 @@ function App() {
       <Button label="Warrior Bee" variant="bee" />
       <Button label="Leader Ironhide" variant="ironhide" size="leader" />
       <Button label="Full Width Jazz" variant="jazz" fullWidth />
+      <Button label="Full Width Prime" variant="prime" fullWidth />
       <Button label="Loading..." loading />
     </div>
   );
